2e611849f40136d283014945815ded961937075f
Patrícia Barnabé <patbarnabe5@gmail.com>
2021-07-18 15:28:13 -0300

    config:firestore

--- src/main.js
+++ src/main.js
@@ -1,5 +1,46 @@
 // Este é o ponto de entrada da sua aplicação
 
-import { myFunction } from './lib/index.js';
+// import { myFunction } from './lib/index.js';
 
-myFunction();
+// myFunction();
+
+const email = 'patricia.argetlam@gmail.com';
+const password = '123456';
+
+
+//Login de novos usuários
+
+firebase
+  .auth()
+  .createUserWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Signed in
+    const user = userCredential.user;
+    console.log('deu bom', user)
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+
+    console.log('deu ruim', errorCode, errorMessage);
+    // ..
+  });
+
+
+//Login de usuários existentes
+
+  firebase.auth().signInWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Signed in
+    const user = userCredential.user;
+
+    console.log('logou!')
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+
+    console.log('não logou')
+  });
