fb267cf6fb95d79540276d27cd11104d4896fe67
Karen Freitas <karenfcorrea@gmail.com>
2021-07-19 17:01:52 -0300

    esqueleto html para testes de autenticação

--- src/main.js
+++ src/main.js
@@ -1,18 +1,30 @@
-import feed from './src/pages/feed/index.js'
+import feed from './pages/feed/index.js'
+import signin from './pages/signin/index.js'
 
 // myFunction();
 
-const email = 'karenfcorrea@gmail.com'
-const password = '12345678'
+const signIn = document.getElementById("signIn")
+const mainLogin = document.getElementById("main")
 
-firebase.auth().signInWithEmailAndPassword(email, password)
-  .then((userCredential) => {
-    // Signed in
-    var user = userCredential.user;
-    console.log("logou!")
-    // ...
-  })
-  .catch((error) => {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-  });
+signIn.addEventListener("click", () => {
+  
+  mainLogin.innerHTML= signin()
+  
+})
+
+
+
+// const email = 'karenfcorrea@gmail.com'
+// const password = '12345678'
+
+// firebase.auth().signInWithEmailAndPassword(email, password)
+//   .then((userCredential) => {
+//     // Signed in
+//     var user = userCredential.user;
+//     console.log("logou!")
+//     // ...
+//   })
+//   .catch((error) => {
+//     var errorCode = error.code;
+//     var errorMessage = error.message;
+//   });
--- src/pages/feed/index.js
+++ src/pages/feed/index.js
@@ -1,4 +1,5 @@
 export default () => {
 
-  
+   return alert("oiiii")
+
 }
