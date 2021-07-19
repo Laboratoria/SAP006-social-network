72cdfc9148bc4a86292bfcc3dc763ea42ae10eb7
Karen Freitas <karenfcorrea@gmail.com>
2021-07-19 17:57:22 -0300

    arrumando erros para autenticação

--- src/main.js
+++ src/main.js
@@ -1,21 +1,39 @@
 import feed from './pages/feed/index.js'
 import signin from './pages/signin/index.js'
+import login from './pages/login/index.js'
 
 // myFunction();
 
-const signIn = document.getElementById("signIn")
 const mainLogin = document.getElementById("main")
 
-signIn.addEventListener("click", () => {
-  
+window.addEventListener("load", () => {
+  mainLogin.innerHTML = login()
+  const signIn = document.getElementById("signIn")
+
+  signIn.addEventListener("click", () => {
   mainLogin.innerHTML= signin()
   
+  })
 })
 
 
 
-// const email = 'karenfcorrea@gmail.com'
-// const password = '12345678'
+
+
+// firebase.auth().createUserWithEmailAndPassword(email, password)
+//   .then((userCredential) => {
+//     // Signed in
+//     var user = userCredential.user;
+//     // ...
+//   })
+//   .catch((error) => {
+//     var errorCode = error.code;
+//     var errorMessage = error.message;
+//     // ..
+//   });
+
+
+
 
 // firebase.auth().signInWithEmailAndPassword(email, password)
 //   .then((userCredential) => {
