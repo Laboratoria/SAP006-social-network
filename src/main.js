8297250e9bdc84b248f751e5210ae7e2b4c314ff
Karen Freitas <karenfcorrea@gmail.com>
2021-07-19 14:56:44 -0300

    Script de autenticação

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
-// Este é o ponto de entrada da sua aplicação
+import feed from './src/pages/feed/index.js'
 
-import { myFunction } from './lib/index.js';
+// myFunction();
 
-myFunction();
+const email = 'karenfcorrea@gmail.com'
+const password = '12345678'
+
+firebase.auth().signInWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Signed in
+    var user = userCredential.user;
+    console.log("logou!")
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+  });
