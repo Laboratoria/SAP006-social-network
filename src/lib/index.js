e52ee0e7d64ab83da8cf826e900d516a8e5d661a
Carolina Alvim <caro.alvim@gmail.com>
2021-07-22 13:46:02 -0300

    feat: teste p atualizar

--- src/lib/index.js
+++ src/lib/index.js
@@ -37,3 +37,53 @@ export const asyncSendProfileData= async (name, image) => {
   })
   console.log("deu certo")
 }
+
+export const signInGoogleAccount = () => {
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    
+    .then((result) => {
+    /** @type {firebase.auth.OAuthCredential} */
+    var credential = result.credential;
+
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // ...
+  })
+  .catch((error) => {
+    
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+  
+}
+
+ 
+export const signOut = () => {
+  firebase
+  .auth()
+  .signOut()
+  /*
+  .then(() => {
+    // Sign-out successful.
+  }).catch((error) => {
+    swal.fire({
+      icon: "error",
+      title: error.message,
+    })// An error happened.
+  });
+ */
+}
