e0bc9e8dae905ec5ffb2800b8b1cbd7382594d2f
jjullimayanne <julli.souza@ufob.edu.br>
2021-07-22 01:12:20 -0300

    funcionando a criação de conta embora ainda esteja sem css

--- src/main.js
+++ src/main.js
@@ -69,3 +69,59 @@ myFunction();
     const password = inputPassword.value;
     const btnLogin = document.getElementById('button-login');
     btnLogin.addEventListener('click', authentication);
+
+
+
+   //julli crate account 
+
+
+   
+   
+
+    const criarFirebaseconta = (email, senha, userName) => {
+        firebase.auth().createUserWithEmailAndPassword(email, senha).then((userReturn) => {
+          userReturn.user.updateProfile({
+            displayName: userName,
+          }).then(() => {
+          }, (error) => {
+            alert('Lamento, algo deu errado!', error);
+          });
+        });
+      };
+    
+const criarConta = document.getElementById('send');
+criarConta.addEventListener('click', (event) => {
+   event.preventDefault();
+
+   const name = document.getElementById('name').value;
+   const email = document.getElementById('email').value;
+   const senha = document.getElementById('password').value;
+   criarFirebaseconta(email, senha, name);
+   if (name === '' || email === '' || senha === '') {
+     alert('Por favor preencha todos os campos');
+   } else {
+     alert('Sua conta foi criada com sucesso ');
+     window.history.pushState(null, null, '/');
+  
+   }
+ });
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
