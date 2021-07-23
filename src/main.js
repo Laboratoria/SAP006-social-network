40ac036712222d17a5bc334eb3b57f1b44410abe
Camila Conte <caxconte@gmail.com>
2021-07-23 14:13:02 -0300

    firebase json added

--- src/main.js
+++ src/main.js
@@ -1,5 +1,35 @@
 // Este é o ponto de entrada da sua aplicação
+import { landing } from "./src/lib/landing.js";
 
-import { myFunction } from './lib/index.js';
+const main = document.querySelector("#root");
 
-myFunction();
+window.addEventListener("load", () => {
+  main.appendChild(landing())
+})
+
+
+firebase.auth().createUserWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Signed in
+    var user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ..
+  });
+
+  firebase.auth().signInWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Signed in
+    var user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+  });
+
+
+// myFunction();
