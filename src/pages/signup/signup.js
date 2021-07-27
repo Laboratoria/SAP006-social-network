1b101d7e317f5982c10ecba6982d30df5d920b39
LethsOliveira <lethsoliveira89@gmail.com>
2021-07-27 01:09:55 -0300

    wip tela de login - centralizar imagem com section de login

--- src/pages/signup/signup.js
+++ src/pages/signup/signup.js
@@ -1,25 +1,28 @@
 export const SignUp = () => {
   const root = document.createElement('div');
   root.innerHTML = `
-    <section id='signUp' class='container'>
-    <form id='labelsForSignUp' class='login-signup'>
-
-      <label for='firstName'>Primeiro Nome:</label>
-      <input id='name' type='name' class='inputs'>
-    
-      <label for='lastName'>Último nome:</label>
-      <input id='name' type='name' class='inputs'>
-
-      <label for='email'>E-mail:</label>
-      <input id='email' type='e-mail' class='inputs'>
-    
-      <label for='password'>Senha:</label>
-      <input id='password' type='password' class='inputs'>
-    
-      <button type='button' id='signUpButton' class='btn'>Cadastrar</button>
-
-    </form>
+  <section id='loginPage' class='loginPage'>
+    <section id='loginImage' class='loginImage'>  
+      <img src="img/login-image.png" class='img' alt='veleiro em mar calmo ao por do sol'>
     </section>
+    <section id='signUp' class='container'>
+      <form id='labelsForSignUp' class='login-signup'>
+        <label for='firstName'>Primeiro Nome:</label>
+        <input id='firstName' type='name' class='inputs'>
+      
+        <label for='lastName'>Último nome:</label>
+        <input id='lastName' type='name' class='inputs'>
+
+        <label for='email'>E-mail:</label>
+        <input id='email' type='e-mail' class='inputs'>
+      
+        <label for='password'>Senha:</label>
+        <input id='password' type='password' class='inputs'>
+      
+        <button type='button' id='signUpButton' class='btn'>Cadastrar</button>
+      </form>
+    </section>
+  </section>
   `;
 
   /*const btnSignUp = root.querySelector('#signUpButton');
