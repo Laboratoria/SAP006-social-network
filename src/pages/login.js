ebb00b1dabd81900b191c796da334f96127b2b7a
Aline Andrade <marson.aline@gmail.com>
2021-07-26 21:50:25 -0300

    Iniciando a pagina de cadastro

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,7 +1,34 @@
 export const Login = () => {
     const rootElement = document.createElement("div");
-    rootElement.innerHTML = `<h1> babla </h1>
-    <button id="cadastro"> Cadastre-se </button>`;
+    rootElement.innerHTML = `<div class="container">
+    <div class="card">
+      <img src="img/GOVEG.png" width="200px">
+      <div class="label-float">
+        <input class="login" type="text" id="usuario" placeholder="E-mail">
+        <span class="focus-input100"></span>
+      </div>
+
+      <div class="label-float">
+        <input class="password" type="password" id="senha" placeholder="Senha">
+        <span class="focus-input100"></span>
+      </div>
+
+      <div class="justify-center">
+        <button type="submit" name="botao" id="entrar">ENTRAR</button>
+      </div>
+
+      <div class="justify-center">
+        <button type="submit" name="botao" id="google-login"> <img src="./img/google.png" class="google-logo" />Sign in
+          with Google</button>
+      </div>
+
+      <div class="justify-center">
+        <hr>
+      </div>
+      <div class="justify-center">
+        <a id="cadastro" href="#">Cadastre-se</a>
+      </div>
+    </div>`;
 
     console.log(rootElement)
 
