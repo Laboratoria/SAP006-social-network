c8f291003f2d5231cb2d6eec4bf44c4338ade57d
Taiene Yuki <taieneyamazaki@gmail.com>
2021-07-27 16:16:37 -0300

    tela inicial

---
+++ src/pages/tela inicial/LogIn.js
@@ -0,0 +1,26 @@
+export default () => {
+  const loginScreenContainer = document.createElement('div');
+
+  const loginScreenButtons = `
+  <img class="logo" src="image/Logotipo(1).png">
+  
+  <form>
+    <input type="email" id="input-email" placeholder="E-mail">
+    <input type="password" id="input-password" placeholder="Senha">
+
+    <button type="button" id="enter-acc">Entrar</button>
+
+    <div class="signUp-link">
+      <span> Ainda não tem conta? </span> 
+      <a href="#"> CADASTRE-SE </a>
+    </div>
+  </form>
+
+  <span class="divider"> ou entre com </span>
+  <button type="button" class="btn-google"> <span class="google-icon"></span> Google</button>
+  `;
+
+  loginScreenContainer.innerHTML = loginScreenButtons;
+
+  return loginScreenContainer;
+};
