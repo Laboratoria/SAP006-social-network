aacf1abb46c5f335c982d583924adb153cb85b34
Patrícia Barnabé <patbarnabe5@gmail.com>
2021-07-29 01:11:50 -0300

    Último ajuste

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -3,9 +3,7 @@ import { loginWithEmail } from '../../services/index.js';
 export default () => {
     const template = document.getElementById("container").innerHTML =`
       <link rel="stylesheet" href="./pages/Register/style.css" />
-
       <main id="box">
-
           <div class="container">
             <div class="banner">
                 <img src="assets/logo.png" alt="Logo">
@@ -14,26 +12,21 @@ export default () => {
                     <h3 class="subtitle">review de séries</h3>
                 </div>
             </div>
-
               
             <section>
                 <form class="form">
                     <input class="input" id="name" type="name" autocomplete="on" placeholder="🗒  Nome Completo" required>
                     <input class="input" id="email" type="email autocomplete="on" placeholder="✉  E-mail" required>
                     <input class="input" id="password" type="password" autocomplete="on" placeholder="⚙  Senha" required>
-                    <input class="input" id="password-Confirm" type="password" autocomplete="on" placeholder="⚙  Confirmar Senha" required>
+                    <input class="input" id="password-confirm" type="password" autocomplete="on" placeholder="⚙  Confirmar Senha" required>
                     <p id="nonUser"></p>
                 </form>
-
                 <button id="signup-button-register" class="buttons register-button">Cadastrar-se</button>
-
                 <button id="gobackButton" class="goback-button">
                     <img src="./assets/arrow.png" alt="Ícone de Seta"> 
                 </button>
             </section>
-
           </div>
-
       </main>
     `;
 
