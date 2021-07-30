feb0c1fb4244cc29238caf1aeca5a88b79a0a2ff
Tauana Pacheco <tauanapachecopp@gmail.com>
2021-07-30 18:24:28 -0300

    comentários apagados

--- src/routes.js
+++ src/routes.js
@@ -24,30 +24,3 @@
 
 
 
-
-
-
-// import { pageMain } from './main.js';
-// import { lala } from './pages/login/test.js'
-
-// const routes = {
-//     // '/': pageMain,
-//     '/lala': lala,
-// };
-// const element = document.querySelector('#root');
-// const validateHash = (hash) => (hash === '' ? '/' : hash.replace('#', ''));
-
-// const routeRender = () => {
-//     const pages = validateHash(window.location.pathname)
-
-//     element.innerHTML = '';
-//     element.appendChild(routes[pages]());
-// };
-
-// const initRoutes = () => window.addEventListener('hashchange', routeRender);
-
-// window.addEventListener('load', () => {
-//     routeRender()
-//     initRoutes();
-// });
-// window.addEventListener('popstate', routeRender);
