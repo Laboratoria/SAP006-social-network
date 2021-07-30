767a38b70e544f8e2d0008ecc808035ac3cceb67
nessacl-95 <vanessalima951@gmail.com>
2021-07-30 02:32:28 -0300

    fix: google authentication

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { renderPage } from './routes.js';
+import {loginPersistence} from './lib/authentication.js';
 
+window.addEventListener('load', loginPersistence());
 window.addEventListener('popstate', renderPage);
 window.addEventListener('load', renderPage);
