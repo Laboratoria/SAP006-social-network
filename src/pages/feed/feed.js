20ff99301f48f489c65338a10c69a135b4c9e785
Lediane <lediane141@gmail.com>
2021-07-31 11:55:07 -0300

    iniciando posts

--- src/pages/feed/feed.js
+++ src/pages/feed/feed.js
@@ -1,9 +1,52 @@
+
 export const Feed = () => {
   const rootElement = document.createElement("div");
-  rootElement.className = "login-container"
+  rootElement.className = "feed-container"
   rootElement.innerHTML = `
-  <h1> FEEEEEEEEED </h1>
+<form action = "" id="postForm">
+  <textarea id='postText' placeholder='O que você quer compartilhar?'></textarea>
+  <button type="submit" id='publicar' class='btn btn-small publish-btn purple'>Publicar</button>
+</form>
+<section id='postado' class='posts-container'>
+
 `
+rootElement.querySelector('#postForm').addEventListener('submit', function(event){
+event.preventDefault();
+const text = rootElement.querySelector('#postText').value;
+
+const post = {
+  text: text,
+  user_id: firebase.auth().currentUser.email,
+  likes: [],
+  comments: []
+}
+const postsCollection = firebase.firestore().collection("posts");
+postsCollection.add(post);
+})
+
+function addPost(post){
+  const postTemplate = `
+  <div id="${post.id}" class="div-postados">
+  ${post.data().text}
+  </div>
+  `
+rootElement.querySelector("#postado").innerHTML += postTemplate
+}
+
+function loadPosts() {
+  const postsCollection = firebase.firestore().collection("posts")
+  postsCollection.get().then(snap => {
+    snap.forEach(post => {
+      addPost(post);
+    })
+  })
+}
+loadPosts();
 
 return rootElement;
  }
+
+
+
+
+
