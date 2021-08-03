7c6977a1556ea9c1c7c178e65a12b084127a7bd9
Aline Andrade <marson.aline@gmail.com>
2021-08-03 15:44:53 -0300

    Cadastro completo

---
+++ src/pages/home/index.js
@@ -0,0 +1,27 @@
+//import { outLogin } from '../../services/firebaseAuth.js';
+
+export const home = () => {
+  const rootElement = document.createElement('div');
+  rootElement.innerHTML = ` 
+  <div class='containerHome'>
+ 
+    <img class='logoHome' src='img/VEG&TAL.png' alt='VEG&TAL - logo'>
+
+    <hr>
+    <h3> botões </h3>
+    <hr>
+    
+<h1> deu certo home rotas!! </h1>
+
+<button class='btn ' id='btnLogout'>Sair</button>
+`;
+
+  // botão sair para fazer logout
+  const btnLogout = rootElement.querySelector('#btnLogout');
+  btnLogout.addEventListener('click', (event) => {
+    event.preventDefault();
+    outLogin();
+  });
+
+  return rootElement;
+};
--- src/router.js
+++ src/router.js
@@ -1,23 +1,24 @@
-import { login } from './pages/login/index.js'
+import { login } from './pages/login/index.js';
 import { cadastro } from './pages/cadastro/index.js';
+import { home } from './pages/home/index.js';
 
 const routRender = () => {
-  const elemento = document.getElementById("root");
+  const elemento = document.getElementById('root');
   const routes = {
-    "/":login,
-    "/cadastro":cadastro
+    '/': login,
+    '/cadastro': cadastro,
+    '/home': home,
 
-  }
-  elemento.innerHTML = "";
-  elemento.appendChild(routes[window.location.pathname]())
- /* elemento.appendChild(routes["abacaxi"]())*/
-  console.log(window.location.pathname)
-}
+  };
+  elemento.innerHTML = '';
+  elemento.appendChild(routes[window.location.pathname]());
+  /* elemento.appendChild(routes["abacaxi"]()) */
+  console.log(window.location.pathname);
+};
 
-window.addEventListener("popstate", routRender);
-window.addEventListener("load", () => {
-  /*window.history.pushState({},"","/")*/
+window.addEventListener('popstate', routRender);
+window.addEventListener('load', () => {
+  /* window.history.pushState({},"","/") */
   routRender();
-  console.log("caiuuuuu no load")
-
-} )
+  console.log('caiuuuuu no load');
+});
