af724aa21ea38c7ff8bf4448b758fe7efb188174
Lediane <lediane141@gmail.com>
2021-08-02 23:34:40 -0300

    ajustes css feed e iniciando editar post

--- src/pages/feed/feed.js
+++ src/pages/feed/feed.js
@@ -6,14 +6,39 @@ export const Feed = () => {
   const rootElement = document.createElement("div");
   rootElement.className = "feed-container"
   rootElement.innerHTML = `
- 
+  <aside class="aside" id="aside-um">
+  <div class="icon-principal">
+  <img src="./images/name-icon.png" alt="">
+</div>
+  <section class='profile-area'>
+    <figure class='profile-area-photo-box'>
+      <img class = "photo" id = "photo">
+      <input required type="file" id="input-file-profileImg" class='input-file-profileImg transparency'
+        accept=".jpg, .jpeg, .png">
+        </section>
+  <div class="icons">
+    <img src="./images/icon-home.png" alt="">
+    <span class="text-icon">Inicio</span>
 
+    <img src="./images/icon-settings.png" alt="">
+    <span class="text-icon">Perfil </span>
+
+    <img src="./images/icon-dark.png" alt="">
+    <button class="text-icon" id="button-dark"> Modo escuro </button>
+    <form action="" id="postForm">
+
+    <img src="./images/icon-dark.png" alt="">
+    <button class="text-icon" id="button-signout"> Sair </button>
+    <form action="" id="postForm">
+      </div>
+</aside>
 
 <form action = "" id="postForm">
   <textarea id='postText' placeholder='O que você quer compartilhar?'></textarea>
   <button id='publicar'>Publicar</button>
 </form>
-<section id='postado' class='posts-container'>
+<section id='postado' class='posts-container'></section>
+
 
 `
 rootElement.querySelector('#postForm').addEventListener('submit', function(event){
@@ -46,10 +71,15 @@ function addPost(post){
   <div id="${post.id}" class="div-postados">
     <p class="user-post">Postado por ${post.data().user_id} </br>
     ${post.data().data} </p>
-    <p class="txt-post">${post.data().text}</p>
+    <p class="txt-post">${post.data().text} </p>
+    <div class="edit-area" style = "display:none;">
+    <textarea id="edit-area" class="edit-post"></textarea>
+    <button id="edit-save" class="button-edit-save"> Salvar </button>
+    </div>
     <section class="likes-comments-bar">
       <div class="icones" id="icone-like"><img src="./images/like.png"> ${post.data().likes}</div>
       <div class="icones" id="icone-comment"><img src="./images/comment.png">  ${post.data().comments} </div>
+      <button id="editar" class="edit-button" value="${post.id}">Editar</button>
       <button id="deletar" class="delete-button" value="${post.id}"> Deletar</button>
     </section>
     
@@ -61,7 +91,7 @@ rootElement.querySelector("#postado").innerHTML += postTemplate;
 };
 
 function loadPosts() {
-  const postsCollection = firebase.firestore().collection("posts")
+  const postsCollection = firebase.firestore().collection("posts").orderBy('data', 'desc')
   postsCollection.get().then(snap => {
     snap.forEach(post => {
       addPost(post);
@@ -78,6 +108,16 @@ function loadPosts() {
           return false;
       })
     })
+
+    /*const editButtons = rootElement.querySelectorAll(".edit-button")
+    editButtons.forEach(button => {
+      button.addEventListener("click", () =>{
+        const buttonValue = button.value;
+        editPost(buttonValue)
+      })
+    })*/
+
+
     const likeButtons = rootElement.querySelectorAll(".like-button")
     likeButtons.forEach(button => {
       button.addEventListener("click", () => {
@@ -90,7 +130,16 @@ function loadPosts() {
   })
 }
 
-function deletePost(postId){
+
+     
+/*function editPost(editedText, postId){//aqui mudar para auth e exportar
+  const postsCollection = firebase.firestore().collection("posts")
+  postsCollection.doc(postId).update({text : editedText})
+}
+*/
+
+
+function deletePost(postId){//aqui mudar para auth e exportar
   const postsCollection = firebase.firestore().collection("posts")
   postsCollection.doc(postId).delete().then(() => {
     rootElement.querySelector("#postado").innerHTML = "";
@@ -98,7 +147,9 @@ function deletePost(postId){
   })
 }
 
-function likePost(postId) {
+
+
+function likePost(postId) {//aqui mudar para auth e exportar
   const increment = firebase.firestore.FieldValue.increment(1);
   const postsCollection = firebase.firestore().collection("posts").doc(postId)
   postsCollection.update({likes:increment})
