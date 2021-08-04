52d52cdb221cdd52be3445cd6d717cc82a3bb12a
LethsOliveira <lethsoliveira89@gmail.com>
2021-08-04 20:19:35 -0300

    wip tela de perfil

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,14 +1,63 @@
-import { signOut } from "../../services/index.js";
+import { signOut } from '../../services/index.js';
 
 export const Profile = () => {
   const root = document.createElement('div');
   root.innerHTML = `
-  <p>Ola! Crie seu perfil</p>
+	<header>
+		<h3> A Bordo Perfil </h3>
+	</header>
+	<main class='profile-container'>
+		<section class='profile-image-container'>
+			<label class='label-image'>
+      <input type='file'>
+      <figure class='profile-figure'>
+        <img src='img/avatar.png' class='avatar-image' alt='avatar'>
+        <figcaption class='avatar-figcaption'>
+          <img src='img/camera-figcaption.png'    >
+        </figcaption>
+      </figure>
+		</section>
 
-  <button type='button' id='buttonSignOut' class='btn-login form-item'>Sair</button>
+		<section class='profile-form'>
+			<form>
+				<fieldset class='fieldset-container'>
+					<legend> Seu Perfil </legend>
+					<div class='form-fields'>
+						<p>Apelido:
+							<input id='surname' type='name' class='input-profile'>
+            </p>
+
+            <p>Nome Completo:
+              <input id='name' type='name' class='input-profile'>
+            </p>
+
+            <p>Localização:
+              <input id='localization' type='localization' class='input-profile'>
+            </p>
+
+            <p>Nome e Modelo do Barco:
+              <input id='boat' type='name' class='input-profile'>
+            </p>
+
+            <p>Email:
+              <input id='email' type='name' class='input-profile'>
+            </p>
+				  </div>
+        </fieldset>
+      </form>
+    </section>
+    
+    <nav class='btn-profile-container'>
+      <button type='button' id='SaveBtn' class='btn-save'>Salvar</button>
+
+      <button type='button' id='buttonSignOut' class='btn-logout'>Sair</button>
+    </nav
+  </main>
+
   
   `;
 
+  const avatarPhoto = root.querySelector ('.avatar-image').value
   const btnSignOut = root.querySelector('#buttonSignOut');
 
   btnSignOut.addEventListener('click', () => {
