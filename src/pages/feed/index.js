6df3f3e91777fee10f0219f1e63abcd5177092ea
Camila Conte <caxconte@gmail.com>
2021-08-04 17:38:23 -0300

    resolvendo conflitos

--- src/pages/feed/index.js
+++ src/pages/feed/index.js
@@ -1,33 +1,29 @@
 export const Feed = () => {
-    const rootElement = document.createElement("div");
-    const container = `
-        <div>
-            <img id="background" src="./pages/login/img/paleta3.jpg" alt="">
-            <div class="esmaeceHeader logotipo-text">
-                <section>
-                <h2>FORT FEED</h2>
-            </div> 
+  const rootElement = document.createElement("div");
+  const container = `
+    <img id="background" src="./pages/login/img/paleta3.jpg" alt="">
+    <div class="esmaeceHeader logotipo-text">
+      <section>
+      <h2>FORT FEED</h2>
+    </div> 
 
-            <section class="post">
-                <form action="" id="published-form">
-                <input type="text" id="text-post" placeholder="Mana, o que você quer compatilhar?">
-                <button id="send-post">Enviar</button>
-                </form>
-            </section>
-        </div> 
-    `;
+    <section class="post">
+      <form action="" id="published-form">
+      <input type="text" id="text-post" placeholder="Mana, o que você quer compatilhar?">
+      <button id="send-post">Enviar</button>
+      </form>
+    </section>
+  `;
 
-    rootElement.innerHTML = container;
+  rootElement.innerHTML = container;
 
-    rootElement.querySelector('#published-form').addEventListener('submit', event => {
-        event.preventDefault();
-        const text = rootElement.querySelector('#text-post').value; 
-        console.log(text);
-    })
+  rootElement.querySelector('#published-form').addEventListener('submit', event => {
+      event.preventDefault();
+      const text = rootElement.querySelector('#text-post').value; 
+      console.log(text);
+  })
 
+  rootElement.querySelector('send-post')
 
-
-    rootElement.querySelector('send-post')
-
-    return rootElement;
+  return rootElement;
 }
