92c99f1878b6db6ef118d79985a4def984847f12
belobruna <bruna.belo@gmail.com>
2021-08-05 14:35:41 -0300

    resolvendo conflitos

--- src/main.js
+++ src/main.js
@@ -8,14 +8,14 @@ const email = 'bruna.belo@gmail.com';
 const password = '123456';
 
 firebase.auth().createUserWithEmailAndPassword(email, password).then((userCredential) => {
-        // Signed in
-        const user = userCredential.user;
-        console.log('deu bom', user);
-        // ...
-    })
-    .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // ..
-        console.log('deu ruim',errorCode, errorMessage);
-    });
+  // Signed in
+  const user = userCredential.user;
+  console.log('deu bom', user);
+  // ...
+})
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+    console.log('deu ruim',errorCode, errorMessage);
+  });
