b03888e1e33b30bf9d2bc8e00ab357eedc5f387a
Tauana Pacheco <tauanapachecopp@gmail.com>
2021-08-06 16:47:14 -0300

    componente header incluído na página de perfil

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,10 +1,13 @@
+import header from '../../components/header/index.js';
+
 export default () => {
   const perfilContainer = document.createElement('div');
+  perfilContainer.append(header());
   const perfilContent = `
-        <img class="logo" src="image/Logotipo(1).png">
-        
-        <p>perfilllll</p>
-        `;
-  perfilContainer.innerHTML = perfilContent;
+    <img class="logo" src="image/Logotipo(1).png">
+    
+    <p>perfilllll</p>
+    `;
+  perfilContainer.innerHTML += perfilContent;
   return perfilContainer;
 };
