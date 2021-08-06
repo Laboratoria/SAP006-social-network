59d98125c0307802883cf359d92cc830e6743e3e
Jéssica Inamassu <inamassu.kaori@gmail.com>
2021-08-06 15:53:34 -0300

    Iniciando SAP

--- src/main.js
+++ src/main.js
@@ -4,6 +4,57 @@
 
 // myFunction();
 
+import homePage from './pages/home-page/index.js';
+import register from './pages/register/index.js';
+import login from './pages/login/index.js';
+import feed from './pages/feed/index.js';
+
+const main = document.querySelector('#root');
+
+const init = () => {
+  window.addEventListener('hashchange', () => {
+    main.innerHTML = '';
+    switch (window.location.hash) {
+      case '#home-page':
+        main.appendChild(homePage());
+        break;
+      case '#register':
+        main.appendChild(register());
+        break;
+      case '#login':
+        main.appendChild(login());
+        break;
+      case '#feed':
+        main.appendChild(feed());
+        break;
+      default:
+        main.appendChild(homePage());
+    }
+  });
+};
+
+window.addEventListener('load', () => {
+  main.innerHTML = '';
+  main.appendChild(homePage());
+  init();
+});
+
+window.addEventListener('click', () => {
+  main.appendChild(register());
+});
+
+// window.addEventListener('click', () => {
+//   main.appendChild(login());
+//   init();
+// });
+
+// window.addEventListener('click', () => {
+//   main.appendChild(feed());
+//   init();
+// });
+
+// Firebase
+
 const email = 'bruna.belo@gmail.com';
 const password = '123456';
 
@@ -17,5 +68,5 @@ firebase.auth().createUserWithEmailAndPassword(email, password).then((userCreden
     const errorCode = error.code;
     const errorMessage = error.message;
     // ..
-    console.log('deu ruim',errorCode, errorMessage);
+    console.log('deu ruim', errorCode, errorMessage);
   });
