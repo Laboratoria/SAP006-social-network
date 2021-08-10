1b91c1c4cc046a0b1cd875895c968623e3093d01
bruna-devbio <bruna.ppp8@gmail.com>
2021-08-10 10:21:43 -0300

    Metodo update ok

--- src/pages/feed/index.js
+++ src/pages/feed/index.js
@@ -28,13 +28,119 @@ export const Feed = () => {
       likes: 0,
       comments: [],
     };
+
     const collectionOfPosts = firebase.firestore().collection('posts');
 
-    collectionOfPosts.add(post).then(() =>{
-      rootElement.querySelector('#text-post') = "";
-      rootElement.querySelector('#get-post'). innerHTML = "";
-    });
-  });
+    collectionOfPosts.add(post).then(() => {
+      rootElement.querySelector("#text-post").value = "";
+      rootElement.querySelector("#get-post").innerHTML = "";
 
-  return rootElement;
+    })
+
+    function addPost(post) {
+
+      const areaOfPost = document.createElement('div')
+      areaOfPost.innerHTML =
+        `
+      <section class="all-posts">
+        <div class='text-published'>${post.data().text} ❤️ ${post.data().likes}</div>
+      </section>
+    
+      <section class='edit-text'>
+    
+        <input class="area-edit" />
+    
+        <div class='save-btn-area'>
+          <button class="save-button" value='${post.id}'>Salvar post</button>
+        </div>
+    
+      </section>
+    
+      <section class="actions-posts">
+    
+        <div class='delete-btn-area'>
+          <button id="delete" class="delete-button" value='${post.id}'>Excluir post</button>
+        </div>
+    
+        <div class='edit-btn-area'>
+          <button id="edit" class="edit-button" value='${post.id}'>Editar post</button>
+        </div>
+    
+      </section>
+   `;
+
+
+      areaOfPost.querySelector('.edit-button').addEventListener('click', () => {
+        const valueInput = areaOfPost.querySelector('.area-edit').value
+        firebase.firestore().collection('posts').doc(post.id).update({ text: valueInput })
+          .then(() => {
+            const containerEditText = areaOfPost.querySelector('.edit-text')
+            containerEditText.style.display = "block"
+            const areaForEdit = areaOfPost.querySelector('.area-edit')
+            const divTextPublished = areaOfPost.querySelector('.text-published')
+            const textReady = areaOfPost.querySelector('.text-published').innerHTML
+
+            divTextPublished.style.display = 'none'
+
+            areaForEdit.value = textReady;
+          })
+          .catch((error) => {
+            console.log('Não foi', error)
+          })
+
+      })
+
+      areaOfPost.querySelector('.save-button').addEventListener('click', () => {
+        const valueInput = areaOfPost.querySelector('.area-edit').value
+        const divTextPublished = areaOfPost.querySelector('.text-published')
+        areaOfPost.querySelector('.text-published')
+        const containerEdit = areaOfPost.querySelector('.edit-text')
+
+        containerEdit.style.display = 'none'
+        divTextPublished.style.display = 'block'
+
+        divTextPublished.innerHTML = valueInput
+      })
+
+      return areaOfPost
+
+    }
+
+    function loadPosts() {
+      const collectionOfPosts = firebase.firestore().collection('posts');
+      collectionOfPosts.get().then(snap => {
+        snap.forEach(post => {
+          const elementPost = addPost(post);
+          rootElement.querySelector('#get-post').appendChild(elementPost)
+        })
+      })
+    }
+
+    const btnDelete = rootElement.querySelectorAll('.delete-button')
+    btnDelete.forEach(buttonExclused => {
+      buttonExclused.addEventListener('click', () => {
+        const valueBtn = buttonExclused.value;
+        console.log(valueBtn);
+        const confirmDelete = confirm("Você deseja excluir o post definitivamente?");
+        if (confirmDelete)
+          deletePost(valueBtn);
+        else
+          return false;
+      })
+    })
+
+
+    function deletePost(postId) {
+      const collectionOfPosts = firebase.firestore().collection('post')
+      collectionOfPosts.doc(postId).delete().then(() => {
+        rootElement.querySelector("#get-post").innerHTML = "";
+        loadPosts();
+      })
+    }
+
+    loadPosts();
+  })
+
+  return rootElement
 };
+
