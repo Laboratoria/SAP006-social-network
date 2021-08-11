a0ef0e6da1e98f3ceb48a6223793a329dc721a17
Taiene Yuki <taieneyamazaki@gmail.com>
2021-08-11 17:21:21 -0300

    exclusao do css#root - print das receitas com quebra de linha

--- src/pages/feed/index.js
+++ src/pages/feed/index.js
@@ -1,17 +1,62 @@
+import { loadRecipe } from '../../services/index.js';
+
 export default () => {
-  const feedConteiner = document.createElement('div');
-  const feedContent = `
-    <img class="logo" src="image/Logotipo(1).png">
-    
-    <p>feeeeeddd</p>
-    <button id="perfilPage">Ir para perfil</button>
+  const feedContainer = document.createElement('div');
+  feedContainer.setAttribute('class', 'screenContainer');
+
+  // const feedContent = `
+
+  //   `;
+
+  // feedContainer.innerHTML = feedContent;
+
+  function addPost(post) {
+    const postTemplate = `
+    <div id=${post.id} class="div-width85 div-recipe">
+
+        <h3 class="title recipe-title"> ${post.data()['nome da receita']} </h3>
+      
+        <div> IMAGEM </div>
+        
+        <div class="recipeBody hide"> 
+          
+          <h3 class="title center">Ingredientes</h3>
+          <div class="">
+            ${post.data().ingredientes}
+          </div>
+          
+          <h3 class="title center">Modo de preparo</h3>
+          <div>
+            ${post.data()['modo de preparo']} 
+          </div>
+
+        </div>
+
+        <div id="recipe-footer" class="div-width100 recipe-title">
+          <span class="material-icons favoriteIcon">favorite</span> ${post.data().likes}
+          <span class="material-icons commentIcon">insert_comment</span></buton> ${post.data().comments.length}
+        </div>
+   
+    </div>
     `;
 
-  feedConteiner.innerHTML = feedContent;
+    feedContainer.innerHTML += postTemplate;
 
-  const perfilButton = feedConteiner.querySelector('#perfilPage');
-  perfilButton.addEventListener('click', () => {
-    window.location.hash = '#profile';
-  });
-  return feedConteiner;
+    const postContainer = feedContainer.querySelectorAll('.div-recipe');
+    console.log(postContainer);
+
+    // for (post of postContainer)
+    // postContainer
+
+    // postContainer.forEach((div) => {
+    //   // const id = div.querySelector('.recipeBody');
+    //   div.addEventListener('click', (div) => {
+    //     div.querySelector('.recipeBody').classList.toggle('show');
+    //   });
+    // });
+  }
+
+  loadRecipe(addPost);
+
+  return feedContainer;
 };
--- src/pages/signUp/index.js
+++ src/pages/signUp/index.js
@@ -2,26 +2,28 @@ import { signUp, userData } from '../../services/index.js';
 
 export default () => {
   const signUpScreenContainer = document.createElement('div');
-  signUpScreenContainer.setAttribute('class', 'container');
+  signUpScreenContainer.setAttribute('class', 'screenContainer');
 
   const signUpForm = `
-  <img class="logo" src="image/logotipo.png">
-  
-  <form id="signUp-form" class="initialForm">
-    <h1 class="title">Criar conta</h1>
+  <div class="div-width85 verticalCenter">
+    <img class="logo" src="image/logotipo.png">
     
-    <input type="text" class="signUp-input" id="signUp-name" placeholder="Nome do usuário" data-required-message-error:"Escreva um nome de usuário" required>
+    <form id="signUp-form" class="initialForm">
+      <h1 class="title">Criar conta</h1>
+      
+      <input type="text" class="signUp-input" id="signUp-name" placeholder="Nome do usuário" data-required-message-error:"Escreva um nome de usuário" required>
+      
+      <input type="email" class="signUp-input" id="signUp-email" placeholder="E-mail" minlength="6" data-required-message-error:"Escreva um email válido" required>
     
-    <input type="email" class="signUp-input" id="signUp-email" placeholder="E-mail" minlength="6" data-required-message-error:"Escreva um email válido" required>
-   
-    <input type="password" class="signUp-input" id="signUp-password" placeholder="Senha (mín 6 caracteres)" required>
-   
-    <input type="password" class="signUp-input" id="repeat-password" placeholder="Repita sua senha" required>
-    <div id="notice" class="notice"> </div>
+      <input type="password" class="signUp-input" id="signUp-password" placeholder="Senha (mín 6 caracteres)" required>
+    
+      <input type="password" class="signUp-input" id="repeat-password" placeholder="Repita sua senha" required>
+      <div id="notice" class="notice"> </div>
 
-    <button type="button" id="btn-signUp" class="btn-login">Cadastrar</button>
-    
-  </form>
+      <button type="button" id="btn-signUp" class="btn-login">Cadastrar</button>
+      
+    </form>
+  </div>
   `;
 
   signUpScreenContainer.innerHTML = signUpForm;
