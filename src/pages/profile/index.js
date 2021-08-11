026877be78c0a054930ee53d398c0a1080c09bd6
Tauana Pacheco <tauanapachecopp@gmail.com>
2021-08-11 17:23:20 -0300

    componente header adicionada

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,25 +1,32 @@
 import header from '../../components/header/index.js';
+import footer from '../../components/footer/index.js';
 
 export default () => {
   const perfilContainer = document.createElement('div');
+  perfilContainer.classList.add('div-profile');
   perfilContainer.append(header());
+
+  const perfilSection = document.createElement('section');
+  perfilSection.classList.add('profile-nav-bar');
+
   const perfilContent = `
-        <section class="profile-nav-bar">
-          <div class="profileInfo">
-           <img class="profilePicture" src="../../image/chef.png">
-            <section>
-              <p>Thais Fernandes</p>
-              <p>Nível: Queima-panela</p>
-            </section>
-          </div>
-          <div id="pofilePages">
-            <ul>
-               <a  class="recipes" href="#myRecipes"> <li class="profile-nav-bar-options">Minhas receitas</li> </a>
-               <a  class="editProfile" href="#profileInfo"> <li class="profile-nav-bar-options">Editar perfil</li> </a>
-            </ul>
-          </div>
+      <div class="profileInfo">
+        <img class="profilePicture" src="../../image/chef.png">
+        <section>
+          <p>Thais Fernandes</p>
+          <p>Nível: Queima-panela</p>
         </section>
-        `;
-  perfilContainer.innerHTML = perfilContent;
+      </div>
+      <div id="pofilePages">
+        <ul>
+            <a  class="recipes" href="#myRecipes"> <li class="profile-nav-bar-options">Minhas receitas</li> </a>
+            <a  class="editProfile" href="#profileInfo"> <li class="profile-nav-bar-options">Editar perfil</li> </a>
+        </ul>
+      </div>
+  `;
+  perfilSection.innerHTML = perfilContent;
+  perfilContainer.append(perfilSection);
+  perfilContainer.append(footer());
+
   return perfilContainer;
 };
