168beb944baf5334d7f0aec9b41ea5f2f5b30ee0
Beatriz Ferraz <beaferraz2010@gmail.com>
2021-08-11 23:05:36 -0300

    configuraçao fire base

--- src/main.js
+++ src/main.js
@@ -1,5 +1,22 @@
 // Este é o ponto de entrada da sua aplicação
 
-import { myFunction } from './lib/index.js';
+//import { myFunction } from './lib/index.js';
 
-myFunction();
+//myFunction();
+
+const email = 'beaferraz@gmail.com';
+const password = '123456';
+
+firebase.auth().createUserWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Signed in
+    const user = userCredential.user;
+    console.log('deu bom', user);
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+    console.log('deu ruim', erroCode, erroMessage);
+  });
