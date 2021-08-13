26f0d947559d38d3944ecd6774d2a24853e51424
Jéssica Inamassu <inamassu.kaori@gmail.com>
2021-08-13 20:41:45 -0300

    funções do firebase

--- src/services/index.js
+++ src/services/index.js
@@ -5,12 +5,12 @@
 
 // Criar usuario
 
-export const fireBaseUser = (email, password) => {
+export const createUser = (email, password) => {
   firebase.auth().createUserWithEmailAndPassword(email, password).then((userCredential) => {
     // Signed in
     const user = userCredential.user;
     console.log('deu bom', user);
-  // ...
+    // ...
   })
     .catch((error) => {
       const errorCode = error.code;
@@ -20,16 +20,45 @@ export const fireBaseUser = (email, password) => {
     });
 };
 
+// Login com email cadastrado
+
+export const loginUser = (email, password) => {
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      console.log('apareceu user', user);
+      window.location.hash('./feed');
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log('Erro', errorCode, errorMessage);
+    });
+};
+
 // Login com google
 
 export const loginGoogle = () => {
   const provider = new firebase.auth.GoogleAuthProvider();
   const result = firebase.auth().singInWhithPopUp(provider)
     .then(() => {
-      window.location.assign('./feed');
+      window.location.hash('./feed');
     })
     .catch((error) => {
       console.error(error);
     });
   return result;
+};
+
+// Logout
+
+export const logout = () => {
+  firebase.auth().signOut().then(() => {
+    // Sign-out successful.
+  }).catch((error) => {
+    // An error happened.
+    console.log('Erro', error);
+  });
 };
