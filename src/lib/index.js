7a1672b19ebb542433ac0e47ba692f72162b52de
Juliete Andrade <julie_sp1990@hotmail.com>
2021-08-16 14:48:32 -0300

    Páginas login/cadastro

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,15 @@
 // aqui você exportará as funções que precisa
 
 export const myFunction = () => {
-  // aqui vai seu código
-  console.log('Olá mundo!');
-};
+  const container = document.createElement("div");
+
+  const template = `
+  <h2>Ellas</h2>
+  <p>Aqui você encontra uma filmografia repleta de mulheres incríveis para te inspirar!</p>
+
+  `;
+
+  container.innerHTML = template;
+
+  return container;
+}
--- src/main.js
+++ src/main.js
@@ -1,5 +1,39 @@
-// Este é o ponto de entrada da sua aplicação
 
-import { myFunction } from './lib/index.js';
+import home from './lib/index.js';
 
-myFunction();
+const main = document.querySelector("#root");
+
+const init = () => {
+  window.addEventListener("hashchange", () => {
+    main.innerHTML = ""
+    switch (window.location.hash) {
+      case "":
+        main.appendChild(home());
+        break;
+      default:
+        main.appendChild(home());
+
+    }
+
+  })
+
+  window.addEventListener("load", () => {
+    main.appendChild(home());
+    init();
+
+    //  const email = "julieteandrade1990@gmail.com";
+    //  const password = "123456";
+
+
+    //  firebase.auth().signInWithEmailAndPassword(email, password)
+    //  .then((userCredential) => {
+    //    // Signed in
+    //    const user = userCredential.user;
+    //    // ...
+    //    console.log("Logou");
+    //  })
+    //  .catch((error) => {
+    //    const errorCode = error.code;
+    //    const errorMessage = error.message;
+    //    console.log("não logou");
+  });
