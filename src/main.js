bea560fef4d4028d8a7043a35fdd30662ccd0faa
Pauline Freitas <paulinefrts@gmail.com>
2021-08-16 14:44:45 -0300

    feed.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 // Este é o ponto de entrada da sua aplicação
 
 import { myFunction } from './lib/index.js';
+import feed from "./pages/feed/index.js";
+import perfil from "./pages/perfil/index.js";
+import addPost from "./pages/addPost/index.js";
 
 myFunction();
---
+++ src/pages/perfil/index.js
@@ -0,0 +1,10 @@
+export default () => {
+    const container = document.createElement('div');
+
+    const template = `
+    CONSTRUIR A TEMPLATE
+    
+    `;
+    container.innerHTML = template;
+    return container
+}
