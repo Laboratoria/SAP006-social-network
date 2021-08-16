a740dc75fc8985ab13c34106a22471cf29793daa
Pauline Freitas <paulinefrts@gmail.com>
2021-08-16 16:47:39 -0300

    atualizaçao main

--- src/main.js
+++ src/main.js
@@ -4,5 +4,4 @@ import { myFunction } from './lib/index.js';
 import feed from "./pages/feed/index.js";
 import perfil from "./pages/perfil/index.js";
 import addPost from "./pages/addPost/index.js";
-
 myFunction();
