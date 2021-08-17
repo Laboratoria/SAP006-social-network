c0d00c2004bc532e45b42b289a3cf4c585fd8195
Aline Andrade <marson.aline@gmail.com>
2021-08-17 13:19:02 -0300

    não está pronto

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -71,7 +71,7 @@ export const home = () => {
         <div class='botoes'> 
           <p class="tipo"> ${doc.data().tipo} </p>
 
-          <button type="button" id="like" data-like=${doc.id}><img src="./img/coracao.svg">${doc.data().curtidas.length}</button>
+          <button type="button" id="like" data-like=${doc.id}><img src="./img/coracao.svg">${(doc.data().curtidas) ? doc.data().curtidas.length : '0'}</button>
           <button type="button" class="price" id="price" data-preco> ${doc.data().preco} <img class="likePrice" src="./img/dinAmarelo.svg"> <img class="likePrice" src="./img/dinCinza.svg"></button>
           <div class="coments" id="coments">
             <input class="addComent" id="addComent" placeholder="Comentários"></input> 
