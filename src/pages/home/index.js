dd3d7d93aea2635cc1a36bb83605333165e0642a
Pauline Freitas <paulinefrts@gmail.com>
2021-08-22 15:21:50 -0300

    arquivo routes estruturado

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,13 +1,37 @@
 export default () => {
-    const container = document.createElement("div");
-  
-    const template = `
-    <h2>Ellas</h2>
-    <p>Aqui você encontra uma filmografia repleta de mulheres incríveis para te inspirar!</p>
+  const container = document.createElement("div");
+
+  const template = `
+    <body>
+    <header>
+      <nav>
+        <h1>Ellas</h1>
+      </nav>
+      <div>
+        <h2>
+          Aqui você encontra uma filmografia repleta de mulheres incríveis para
+          te inspirar!
+        </h2>
+      </div>
+    </header>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <a href="/#login">Login</a>
+          </li>
+          <li>
+            <a href="/#cadastre-se">Cadastre-se</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </body>
+    
   
     `;
-  
-    container.innerHTML = template;
-  
-    return container;
-  }
+
+  container.innerHTML = template;
+
+  return container;
+};
