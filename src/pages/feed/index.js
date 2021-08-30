dea9a767399e28c7d315a9b32db4e894fc3891b0
Jéssica Inamassu <inamassu.kaori@gmail.com>
2021-08-30 14:47:48 -0300

    Criação da pagina de feed

--- src/pages/feed/index.js
+++ src/pages/feed/index.js
@@ -2,12 +2,17 @@ export default () => {
   const container = document.createElement('div');
 
   const template = `
-  <a href="/#feed">
-    <img src="img/google-logo.png" alt="" class="logo">
-  </a>
-  <nav class="menu">
-  </nav> 
-  <p>Feed</p>
+    <header>
+      <nav class="menu">
+      </nav> 
+      <a href="/#feed">
+        <img src="img/google-logo.png" alt="" class="logo">
+      </a>
+    </header>
+    <h5>Post</h5>
+      <form action=".">
+        <textarea name="post-feed" id="post" class="post-text" cols="30" rows="5" maxlength="500" placeholder="Escreva sua experiência aqui"></textarea>
+      </form>
   `;
 
   container.innerHTML = template;
