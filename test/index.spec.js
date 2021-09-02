f6b4540b445752b9f75283612d82b44686494e1d
Tauana Pacheco <tauanapachecopp@gmail.com>
2021-09-01 22:45:24 -0300

    inicio dos testes unitários - loginScreen

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,16 +1,46 @@
-// importamos a função que vamos testar
-import { signIn } from '../src/services/index.js';
+/**
+ * @jest-environment jsdom
+ */
 
-describe('myFunction', () => {
-  const mockUser = [
-    { name: 'Danila', password: '7startup20' },
-    { name: 'Nadir', password: 'naruto' },
-    { name: 'Iris', password: 'starttrek' },
-  ];
-  it('should be a function', () => {
-    expect(typeof signIn).toBe('function');
+import loginScreen from '../src/pages/login/index.js';
+import * as services from '../src/services/index.js';
+
+jest.mock('../src/services/index.js');
+
+describe('return sreen login', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
   });
-  it('auth with firebase', () => {
-    expect(mockUser).tobeCalled(signIn(mockUser.name));
+  it('should return loginScreen', () => {
+    // prepara mocks
+    services.signIn.mockResolvedValueOnce(true);
+    // renderiza a tela
+    const screenPage = loginScreen();
+    // executa ações
+    screenPage.querySelector('#input-email').value = 'sasori.com';
+    screenPage.querySelector('#input-password').value = 'areiamarionete';
+    screenPage.querySelector('#enter-acc').click();
+    // valida teste;
+    expect(services.signIn).toHaveBeenCalledTimes(1);
+  });
+  it('should return an error', async () => {
+    await services.signIn.mockRejectedValueOnce({
+      // o que fazer aqui? error: tentativa:
+      // await expect(Promise.resolve().then(() => {
+      //   window.location.hash = '#feed';
+      // })).toBeDefined();
+    });
+    const screenPage = loginScreen();
+    screenPage.querySelector('#input-email').value = 'sasori.com';
+    screenPage.querySelector('#input-password').value = 'areiamarionete';
+    screenPage.querySelector('#enter-acc').click();
+    expect(services.signIn).toHaveBeenCalledTimes(1);
+  });
+  it('should not return loginScreen', () => {
+    const screenPage = loginScreen();
+    screenPage.querySelector('#input-email').value = '';
+    screenPage.querySelector('#input-password').value = '';
+    screenPage.querySelector('#enter-acc').click();
+    expect(services.signIn).not.toHaveBeenCalled();
   });
 });
