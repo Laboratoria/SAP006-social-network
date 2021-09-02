72b515097dabba0a1f35b6a0382b08e3c49f692e
Tauana Pacheco <tauanapachecopp@gmail.com>
2021-09-02 19:00:24 -0300

    teste da pagina loginScreen

--- test/index.spec.js
+++ test/index.spec.js
@@ -11,6 +11,7 @@ describe('return sreen login', () => {
   beforeEach(() => {
     jest.clearAllMocks();
   });
+
   it('should return loginScreen', () => {
     // prepara mocks
     services.signIn.mockResolvedValueOnce(true);
@@ -23,19 +24,6 @@ describe('return sreen login', () => {
     // valida teste;
     expect(services.signIn).toHaveBeenCalledTimes(1);
   });
-  it('should return an error', async () => {
-    await services.signIn.mockRejectedValueOnce({
-      // o que fazer aqui? error: tentativa:
-      // await expect(Promise.resolve().then(() => {
-      //   window.location.hash = '#feed';
-      // })).toBeDefined();
-    });
-    const screenPage = loginScreen();
-    screenPage.querySelector('#input-email').value = 'sasori.com';
-    screenPage.querySelector('#input-password').value = 'areiamarionete';
-    screenPage.querySelector('#enter-acc').click();
-    expect(services.signIn).toHaveBeenCalledTimes(1);
-  });
   it('should not return loginScreen', () => {
     const screenPage = loginScreen();
     screenPage.querySelector('#input-email').value = '';
@@ -43,4 +31,64 @@ describe('return sreen login', () => {
     screenPage.querySelector('#enter-acc').click();
     expect(services.signIn).not.toHaveBeenCalled();
   });
+  it('should return an error', () => {
+    services.signIn.mockRejectedValueOnce({
+      error: {
+        code: 'auth/invalid-email',
+        message: 'Usuário ou email inválido',
+      },
+    });
+    const screenPage = loginScreen();
+    screenPage.querySelector('#input-email').value = 'sasori.com';
+    screenPage.querySelector('#input-password').value = 'areiamarionete';
+    screenPage.querySelector('#enter-acc').click();
+    expect(services.signIn).toHaveBeenCalledTimes(1);
+    // await expect(services.signIn).rejects.toEqual({
+    //   error: {
+    //     code: 'auth/invalid-email',
+    //   },
+    // });
+  });
 });
+
+// it('the fetch fails with an error', () => {
+//   expect.assertions(1);
+//   return services.signIn().catch((e) => expect(e).toMatch('error'));
+// });
+// await services.signIn.mockRejectedValueOnce({
+//   o que fazer aqui? error: tentativa:
+//   await expect(Promise.resolve().then(() => {
+//     window.location.hash = '#feed';
+//   })).toBeDefined();
+// });
+describe('return second function', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+  it('should return function with Google', () => {
+    services.signInWithGoogle.mockResolvedValueOnce(true);
+    const screenPage = loginScreen();
+    screenPage.querySelector('#btn-google').click();
+    expect(services.signInWithGoogle).toHaveBeenCalledTimes(1);
+  });
+  it('dont should return login with Google', () => {
+    services.signInWithGoogle.mockRejectedValueOnce({
+      error: {
+        code: 'auth/invalid-email',
+      },
+    });
+    const screenPage = loginScreen();
+    screenPage.querySelector('#btn-google').click();
+    screenPage.querySelector('#sign-up').click();
+    expect(services.signInWithGoogle).toHaveBeenCalledTimes(1);
+  });
+});
+
+// describe('user out', () => {
+//   beforeEach(() => {
+//     jest.clearAllMocks();
+//   });
+//   it('should return user out the app', () => {
+//     services.signInWithGoogle
+//   });
+// });
