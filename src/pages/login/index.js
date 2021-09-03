1f10913a3e8e459dba88fe8105d311a52e9d026a
Leonardo <leonardo_mbg@hotmail.com>
2021-09-03 18:03:56 -0300

    login botao

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,26 +1,47 @@
-export default() => {
-    const contrainer = document.createElement("div");
+export default () => {
+  const container = document.createElement("div");
 
-    const template = `
-    <h2>petlove</h2>
-    <p>um site para pets repleta de animais incriveis</p>
-    <link rel= stylesheet" href="style.css">
-    <div class=container">
-    <div class="card">
-        <h2>fazer login :</h2>
-        <input type="text" id="usuário" placeholder="">
-        <label for="usuário"</label>
-        < form method='post'>
-        <input required="" autocomplete="off" type='email' placeholder='Email'
-        <input required="" autocomplete="off" type='password' placeholder='Senha'
-        </form>
-        <buton class='button-area sigIn' id='start'>Entrar</button>
-        <p class="or-area"> ou </p>
-        <button class= 'button-area btn btnGoogle'><img src='imagens/google_small_id</button>
-        
-    </div>  
+  const template = `
+  <section class="container">
+        <h2>Login Pet Friend´s</h2>
+        <form action="">
+            <div class="input-field">
+                <input type="text" name="username" id="username"
+                    placeholder="Enter Your Username">
+                <div class="underline"></div>
+            </div>
+            <div class="input-field">
+                <input type="password" name="password" id="password"
+                    placeholder="Enter Your Password">
+                <div class="underline"></div>
+            </div>
+
+           
+
+        <button type="submit" class="button" id="entrar">Entrar</button>
+  </form>
+
+  <div class="footer">
+      <span>Or Connect With Social Media</span>
+    
+          <div class="social-field facebook">
+              <a href="#">
+                  <i class="fab fa-facebook-f"></i>
+                  Sign in with Facebook
+              </a>
+          </div>
+        <p class="p-sign-in">Não tem uma conta? <a href=""
+   class="forget-password-link" id="signup-link">Cadastre-se</a></p>    
+  </div>
+  </div>
+       </section>
         `;
-    contrainer.innerHTML = template;
+  container.innerHTML = template;
 
-    return contrainer;
-}
+  const entrar = container.querySelector("#entrar");
+  entrar.addEventListener("click", () => {
+    window.location.hash = "#cadastro";
+  });
+
+  return container;
+};
