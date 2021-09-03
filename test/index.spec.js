8af4006df2268506ff04b45b6a372fd6c14628e7
Tauana Pacheco <tauanapachecopp@gmail.com>
2021-09-03 17:49:17 -0300

    refatoração do primeiro teste e mais um inserido

--- test/index.spec.js
+++ test/index.spec.js
@@ -3,6 +3,7 @@
  */
 
 import loginScreen from '../src/pages/login/index.js';
+import signUpScreen from '../src/pages/signUp/index.js';
 import * as services from '../src/services/index.js';
 
 jest.mock('../src/services/index.js');
@@ -14,7 +15,16 @@ describe('return sreen login', () => {
 
   it('should return loginScreen', () => {
     // prepara mocks
-    services.signIn.mockResolvedValueOnce(true);
+    services.signIn.mockResolvedValueOnce((email, password) => {
+      const userCredential = {
+        user: {
+          uid: 'sdsadsa',
+          email,
+          password,
+        },
+      };
+      return userCredential;
+    });
     // renderiza a tela
     const screenPage = loginScreen();
     // executa ações
@@ -23,11 +33,21 @@ describe('return sreen login', () => {
     screenPage.querySelector('#enter-acc').click();
     // valida teste;
     expect(services.signIn).toHaveBeenCalledTimes(1);
+    expect(services.signIn).toHaveBeenCalledWith('sasori.com', 'areiamarionete');
+  });
+  it('test', async () => {
+    services.signIn.mockResolvedValueOnce({
+      user: {
+        uid: 'sdsadsa',
+      },
+    });
+    expect.assertions(1);
+    await expect(services.signIn('email', 'password')).resolves.toEqual({ user: { uid: 'sdsadsa' } });
   });
   it('should not return loginScreen', () => {
     const screenPage = loginScreen();
     screenPage.querySelector('#input-email').value = '';
-    screenPage.querySelector('#input-password').value = '';
+    screenPage.querySelector('#input-password').value = 'abacaxi';
     screenPage.querySelector('#enter-acc').click();
     expect(services.signIn).not.toHaveBeenCalled();
   });
@@ -39,28 +59,13 @@ describe('return sreen login', () => {
       },
     });
     const screenPage = loginScreen();
-    screenPage.querySelector('#input-email').value = 'sasori.com';
-    screenPage.querySelector('#input-password').value = 'areiamarionete';
+    screenPage.querySelector('#input-email').value = 'sasori';
+    screenPage.querySelector('#input-password').value = 'marionete';
     screenPage.querySelector('#enter-acc').click();
     expect(services.signIn).toHaveBeenCalledTimes(1);
-    // await expect(services.signIn).rejects.toEqual({
-    //   error: {
-    //     code: 'auth/invalid-email',
-    //   },
-    // });
   });
 });
 
-// it('the fetch fails with an error', () => {
-//   expect.assertions(1);
-//   return services.signIn().catch((e) => expect(e).toMatch('error'));
-// });
-// await services.signIn.mockRejectedValueOnce({
-//   o que fazer aqui? error: tentativa:
-//   await expect(Promise.resolve().then(() => {
-//     window.location.hash = '#feed';
-//   })).toBeDefined();
-// });
 describe('return second function', () => {
   beforeEach(() => {
     jest.clearAllMocks();
@@ -84,11 +89,64 @@ describe('return second function', () => {
   });
 });
 
-// describe('user out', () => {
-//   beforeEach(() => {
-//     jest.clearAllMocks();
-//   });
-//   it('should return user out the app', () => {
-//     services.signInWithGoogle
-//   });
-// });
+describe('should render page register correctly', () => {
+  it('should be div', () => {
+    const divSignUp = signUpScreen();
+    expect(divSignUp.tagName).toBe('DIV');
+  });
+});
+
+describe('should return page signUpScreen', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+  it('', () => {
+    services.signUp.mockResolvedValueOnce((email, password, signUpName) => {
+      const userCredential = {
+        user: {
+          uid: 'abch1276',
+          email,
+          password,
+          signUpName,
+        },
+      };
+      return userCredential;
+    });
+    const screenSignUp = signUpScreen();
+    screenSignUp.querySelector('#signUp-name').value = 'tobe';
+    screenSignUp.querySelector('#signUp-email').value = 'tobe@hotmail.com';
+    screenSignUp.querySelector('#signUp-password').value = '951357';
+    screenSignUp.querySelector('#repeat-password').value = '951357';
+    screenSignUp.querySelector('#btn-signUp').click();
+    expect(services.signUp).toHaveBeenCalled();
+    expect(services.signUp).toHaveBeenCalledWith('tobe@hotmail.com', '951357', 'tobe');
+  });
+  it('should not return loginScreen', () => {
+    const screenSignUp = signUpScreen();
+    screenSignUp.querySelector('#signUp-name').value = '';
+    screenSignUp.querySelector('#signUp-email').value = '';
+    screenSignUp.querySelector('#signUp-password').value = '';
+    screenSignUp.querySelector('#repeat-password').value = '57';
+    screenSignUp.querySelector('#btn-signUp').click();
+    expect(services.signUp).not.toHaveBeenCalled();
+  });
+  it('dont return singUp', () => {
+    services.signUp.mockRejectedValueOnce((email, password, signUpName) => {
+      const test = {
+        user: {
+          email,
+          password: '951375',
+          signUpName,
+        },
+      };
+      return test;
+    });
+    const screenSignUp = signUpScreen();
+    screenSignUp.querySelector('#signUp-name').value = 'tobe';
+    screenSignUp.querySelector('#signUp-email').value = 'tobe@hotmail.com';
+    screenSignUp.querySelector('#signUp-password').value = '951375';
+    screenSignUp.querySelector('#repeat-password').value = '951357';
+    screenSignUp.querySelector('#btn-signUp').click();
+    expect(services.signUp).not.toHaveBeenCalled();
+  });
+});
