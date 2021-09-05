a589794159fc43cec241da724d491da61c0d0bed
Letícia Braga Aniceto <leticia.aniceto@usp.br>
2021-09-05 17:08:01 -0300

    responsividade da reset page para desktop e tablet

--- src/pages/signup/reset.js
+++ src/pages/signup/reset.js
@@ -1,29 +1,30 @@
-import { resetPassword } from "../../services/authentication.js";
+import { resetPassword } from '../../services/authentication.js';
 
 export const Reset = () => {
-  const rootElement = document.createElement("div");
+  const rootElement = document.createElement('div');
   const container = `
-    <img id="background" src="./pages/login/img/paleta3.jpg" alt="">
-    <div class="esmaeceHeader logotipo-text">
-      <section>
-        <h2>FORT</h2>
+    <header class="header-container">
+      <section class="resetSection-header-title">
+        <h2 class="reset-header-title">FORT</h2>
       </section>
-    </div>
-    <h4> Insira seu e-mail abaixo para receber o link de redefinição de senha</h4>
-    <div class="inputAndReset">
-      <input type="email" id="email" class="reset-input-layout" placeholder="Email">
-    </div>
-    <div class="google">
-      <button id="btn-reset" class="login btn">Enviar</button>
-    </div>
+    </header>
+    <section class="resetSection-background-opacity">
+      <h4 class="reset-text">Insira seu e-mail abaixo para receber o link de redefinição de senha</h4>
+      <div class="reset-input">
+        <input type="email" id="email" class="reset-input-layout" placeholder="Email">
+      </div>
+      <div class="container-btn-login">
+        <button id="btn-reset" class="btn-reset login btn">Enviar</button>
+      </div>
+    </section>
     `;
 
   rootElement.innerHTML = container;
 
-  const btnReset = rootElement.querySelector("#btn-reset");
+  const btnReset = rootElement.querySelector('#btn-reset');
 
-  btnReset.addEventListener("click", () => {
-    const email = rootElement.querySelector("#email");
+  btnReset.addEventListener('click', () => {
+    const email = rootElement.querySelector('#email');
     resetPassword(email.value);
   });
 
