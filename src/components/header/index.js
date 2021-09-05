4189e253a9a889a3a825a4d55e14e30e0b832b53
Taiene Yuki <taieneyamazaki@gmail.com>
2021-09-05 17:28:36 -0300

    responsividade-header-footer

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,43 +2,42 @@ import { removeUserData, signOut } from '../../services/index.js';
 
 export default () => {
   const headerContainer = document.createElement('div');
+  headerContainer.setAttribute('id', 'headerContainer');
 
   const headerContent = `
-  <header id="header">
-    <button id="btn-mobile">
-      <span id="hamburger"></span>
-    </button>
-    <h1>FoodLovers</h1>
-    <nav id="nav">
-      <ul id="menu">
-        <li><a href="#feed">Home</a></li>
-        <li><a href="#profile">Perfil</a><li>  
-        <li><a href="#postRecipe">Adicionar receita</a><li>
-        <li id="signOut"><a href="#"><i class="fas fa-sign-out-alt"></i></a><li>
-      </ul>
-    </nav>
-    <!----
-      <div class="icons-header">
-        <i class="fas fa-search"><input id="search"/></i>
-        <i class="fas fa-sign-out-alt"></i>
-      </div>
-      
-    --->
+    <header id="header">
+      <button id="btn-mobile">
+        <span id="hamburger"></span>
+      </button>
 
-    <div id="noticeHeader" class='popup'>
-    <p class="notice-header-p">Desculpe, não foi possível deslogar. Por favor, tente novamente.</p>
-    <button id="btnOk" class="btnOk">Ok</button>
-    </div>
-    <div id="overlay"></div>
-  </header>
-   `;
+      <h1 class="titleFoodLovers">FoodLovers</h1>
+       
+      <span class="searchbox-icon header-icon"> <i class="fas fa-search"></i> </span>
+    
+      <nav id="nav">
+        <ul id="menu">
+          <li><a href="#feed">Home</a></li>
+          <li><a href="#profile">Perfil</a><li>  
+          <li><a href="#postRecipe">Adicionar receita</a><li>
+          <li id="signOut"><a href="#"> Sair <i class="fas fa-sign-out-alt"></i></a><li>
+        </ul>
+      </nav>
+      
+      <div id="noticeHeader" class='popup'>
+      <p class="notice-header-p">Desculpe, não foi possível deslogar. Por favor, tente novamente.</p>
+      <button id="btnOk" class="btnOk">Ok</button>
+      </div>
+      <div class="overlay"></div>
+    </header>
+  `;
+
   headerContainer.innerHTML += headerContent;
 
   const btnHamburger = headerContainer.querySelector('#btn-mobile');
   const nav = headerContainer.querySelector('#nav');
   const btnSignOut = headerContainer.querySelector('#signOut');
   const btnOk = headerContainer.querySelector('#btnOk');
-  const toggle = headerContainer.querySelectorAll('#overlay, #noticeHeader');
+  const toggle = headerContainer.querySelectorAll('.overlay, #noticeHeader');
 
   function toggleMenu() {
     nav.classList.toggle('active');
@@ -51,6 +50,10 @@ export default () => {
   function signinOutDom() {
     signOut().then(() => {
       removeUserData();
+
+      const body = document.querySelector('body');
+      const footerTag = body.querySelector('footer');
+      footerTag.remove();
     }).catch(() => {
       toggleSignOutnotice();
     });
@@ -61,6 +64,12 @@ export default () => {
   btnSignOut.addEventListener('click', signinOutDom);
 
   btnOk.addEventListener('click', toggleSignOutnotice);
+
+  const iconSearch = headerContainer.querySelector('.searchbox-icon');
+
+  iconSearch.addEventListener('click', () => {
+    window.location.hash = '#search';
+  });
 
   return headerContainer;
 };
