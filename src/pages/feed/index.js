f822e2fd62445e67581839c8b5d571f870970c4c
Paulinefreitas <paulinefrts@gmail.com>
2021-09-07 14:36:25 -0300

    criar post

--- src/pages/feed/index.js
+++ src/pages/feed/index.js
@@ -1,11 +1,11 @@
 export default () => {
-    const container = document.createElement('div');
+  const container = document.createElement("div");
 
-    const template = `
+  const template = `
     <header>
       <h1>Ellas</h1>
     </header>
-    <form action="" class="form">
+    <p><form action="" id="post-form" class="form">
       <label for="name-film">Filme</label>
       <input type="text" id="name-film" />
       <label for="img-film">Anexe uma imagem do filme</label>
@@ -29,7 +29,8 @@ export default () => {
         <button class="button-post" id="button-publicar">Publicar</button>
         <button class="button-post" id="button-descartar">Descartar</button>
       </div>
-    </form>
+</form>
+  </p>
 
     <footer id="rodape">
       <nav>
@@ -41,6 +42,25 @@ export default () => {
     </footer>
     
     `;
-    container.innerHTML = template;
-    return container
-}
+  container.innerHTML = template;
+  return container;
+};
+
+//CRIAR POST
+document.getElementById("post-form").addEventListener("submit", function (event){
+  event.preventDefault();
+  const text = document.getElementById("post-text").value;
+  const filmName = document.getElementById("name-film").value;
+  const filmImage = document.getElementById("input-img-film").value;
+  const post = {
+    text: text;
+    img_film: filmImage;
+    name_film: filmName;
+    user_id: "Julio";
+    likes: 0;
+    comments: [];
+  }
+   const postCollection = firebase.firestore().collection("post")        postsCollection.add(post)                                           
+})
+
+//MOSTRAR POST NA TELA
